c458daab71184dcb1547a09d5c2afa019f87a7eb
arthmis <arthmis20@gmail.com>
2020-08-06 18:48:52 -0400

    updating entrypoint install scylla in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
-echo "Hello"
+docker run --name chat-app --hostname localhost -d scylladb/scylla
+docker exec -it some-scylla nodetool status
+docker exec -it some-scylla cqlsh 
+echo "Testing Successful so far"
