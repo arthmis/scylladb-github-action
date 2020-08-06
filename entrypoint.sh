ae23a784dd4bb621bd8f62556d365ddb71c695fa
arthmis <arthmis20@gmail.com>
2020-08-06 19:53:05 -0400

    testing new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
-docker run --name chat-app --hostname localhost -d scylladb/scylla
-docker exec -it some-scylla nodetool status
-docker exec -it some-scylla cqlsh 
+# docker run --name chat-app --hostname localhost -d scylladb/scylla
+# docker exec -it some-scylla nodetool status
+# docker exec -it some-scylla cqlsh 
+docker_run="docker run"
+# docker_run="$docker_run -e"
+sh -c "$docker_run"
 echo "Testing Successful so far"
