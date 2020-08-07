71fed8b9a0050fdaa27462d940952676a2230962
arthmis <arthmis20@gmail.com>
2020-08-06 20:18:41 -0400

    adding port to test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 # docker exec -it some-scylla nodetool status
 # docker exec -it some-scylla cqlsh 
 docker_run="docker run --name chat-app --hostname localhost -d scylladb/scylla"
-docker_run="$docker_run && docker exec chat-app nodetool status"
+docker_run="$docker_run && docker exec chat-app nodetool -p 9042 status"
 # docker_run="$docker_run -e"
 sh -c "$docker_run"
 echo "Testing Successful so far"
